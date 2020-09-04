8f9f2d6c4e81641452f75878eb3326dc329d055c
Claudia Yapuchura <claudiasaire213@gmail.com>
2020-09-03 23:36:18 -0300

    Adicionar método delete dos temas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   { path: 'editar-post/:id', component: PutPostagemComponent },
   { path: 'delete-post/:id', component: DeletePostagemComponent },
   { path: 'editar-tema/:id', component: PutTemaComponent },
-  { path: 'delete-tema/:id', component: DeleteTemaComponent }
+  {path: 'delete-tema/:id', component: DeleteTemaComponent }
 ];
 
 @NgModule({
